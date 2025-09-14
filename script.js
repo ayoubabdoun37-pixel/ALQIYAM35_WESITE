4acce30f4302ebc470cc52f3844b74a6f6e31773
ayoubabdoun37-pixel <ayoubabdoun37@gmail.com>
2025-09-14 12:07:11 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// نموذج التواصل - عرض رسالة تأكيد
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-    alert("تم إرسال رسالتك بنجاح! شكراً لتواصلك معنا.");
-    this.reset();
-});
